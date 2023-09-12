58c4c294fbb1eff49cd8c684d25585ce9ffc1a53
gifte <pavelbahustki@gmail.com>
2023-09-12 16:14:01 +0300

    only counter voite HW

---
+++ src/main/java/org/example/SingerVote/TextTime.java
@@ -0,0 +1,33 @@
+package org.example.SingerVote;
+
+public class TextTime {
+    private String text;
+    private long time;
+
+    public TextTime(String text) {
+        this.text = text;
+        this.time = System.currentTimeMillis();;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+
+    @Override
+    public String toString() {
+        return  "   " + text;
+    }
+}
