e29fd3eff71adba28a6efca994aa6d66e95f541e
gifte <pavelbahustki@gmail.com>
2023-09-12 16:19:22 +0300

    reload HW

--- src/main/java/org/example/SingerVote/CounterVote.java
+++ src/main/java/org/example/SingerVote/CounterVote.java
@@ -1,1 +1,45 @@
 package org.example.SingerVote;
+
+public class CounterVote {
+    String subjectOfVote;
+    int count = 1;
+    CounterVote (String name){
+    this.subjectOfVote = name;
+    }
+
+    CounterVote (String name, int count){
+        this.subjectOfVote = name;
+        this.count = count;
+    }
+
+    public String getSubjectOfVote() {
+        return subjectOfVote;
+    }
+    public void setSubjectOfVote(String subjectOfVote) {
+        this.subjectOfVote = subjectOfVote;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void increment() {
+        this.count ++;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof CounterVote)) {
+            return false;
+        }
+        CounterVote other =(CounterVote)object;
+        return subjectOfVote.equals(other.subjectOfVote);
+    }
+    @Override
+    public String toString() {
+        return "( it is  " + subjectOfVote  +
+                "  vote:   " + count + ')';
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
+
